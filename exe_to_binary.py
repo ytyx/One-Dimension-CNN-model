86382680bd6973d17fbf3967d09f5c10fd7c4e1c
Tianyunxi(Emily) Yin <emilyyin0322@gmail.com>
2024-03-13 10:45:36 -0700

    Update exe_to_binary.py

--- exe_to_binary.py
+++ exe_to_binary.py
@@ -1,64 +1,26 @@
-# This file takes .exe folders and convert exe files into byte sequence
-
 import os
 import math
 import argparse
 from PIL import Image
 from queue import Queue
 from threading import Thread
+import py7zr
 
 
 def getBinaryData(filename):
     """
-    Extract byte values from binary executable file and store them into list
-    :param filename: executable file name
-    :return: byte value list
+    Extract byte values from binary executable file
     """
 
     with open(filename, 'rb') as fileobject:
 
         # read file byte by byte
-        data = fileobject.read(1)
+        data = fileobject.read()
 
-        while data != b'':
-            data = fileobject.read(1)
+        # while data != b'':
+        #     data = fileobject.read(1)
 
     return data
-
-
-def get_size(data_length, width=None):
-
-    if width is None:
-
-        size = data_length
-
-        if (size < 10000):
-            width = 1024
-        elif (10000 <= size <= 10000 * 3):
-            width = 4096
-        elif (10000 * 3 <= size <= 10000 * 6):
-            width = 128 * 128
-        elif (10000 * 6 <= size <= 10000 * 10):
-            width = 256 * 256
-        elif (10000 * 10 <= size <= 10000 * 20):
-            width = 384 * 384
-        elif (10000 * 20 <= size <= 10000 * 50):
-            width = 512 * 512
-        elif (10000 * 50 <= size <= 10000 * 100):
-            width = 768 * 768
-        else:
-            width = 1024 * 1024
-
-    return width
-
-
-def resize_binary_data(binary_data, target_length):
-    binary_data = bytes(binary_data)
-    if len(binary_data) >= target_length:
-        return binary_data[:target_length]
-    else:
-        padding = bytes(target_length - len(binary_data))
-        return binary_data + padding
 
 
 def save_binary_data(filename, binary_data):
@@ -66,7 +28,7 @@ def save_binary_data(filename, binary_data):
         dirname = os.path.dirname(filename)
         name, _ = os.path.splitext(filename)
         name = os.path.basename(name)
-        folder_path = "/data/tiny_binary_files/benign" \
+        folder_path = "/Users/yintianyunxi/Desktop/Malware_1D_CNN/raw/malware" \
             + os.sep + name + '.bin'
         os.makedirs(os.path.dirname(folder_path), exist_ok=True)
 
@@ -77,18 +39,18 @@ def save_binary_data(filename, binary_data):
         print("Error", err)
 
 
-def run(file_queue, width):
+def run(file_queue):
 
     while not file_queue.empty():
         filename = file_queue.get()
         binary_data = getBinaryData(filename)
-        target_length = get_size(len(binary_data), width=None)
-        resized_data = resize_binary_data(binary_data, target_length)
-        save_binary_data(filename, resized_data)
+        # target_length = get_size(len(binary_data), width=None)
+        # resized_data = resize_binary_data(binary_data, target_length)
+        save_binary_data(filename, binary_data)
         file_queue.task_done()
 
 
-def main(input_dir, width=None, thread_number=7):
+def main(input_dir, thread_number=7):
     # Get all executable files in input directory and add them into queue
     file_queue = Queue()
     for root, directories, files in os.walk(input_dir):
@@ -104,5 +66,11 @@ def main(input_dir, width=None, thread_number=7):
         thread.start()
     file_queue.join()
 
+# zip_file_path = 'path/of/zipfile.7z'
 
-main('/Users/yintianyunxi/Desktop/Benign-NET/cnetnet')
+# with py7zr.SevenZipFile(zip_file_path, mode='r', password='infected') as archive:
+#     # Extract the contents of the 7z file to the specified directory
+#     archive.extractall(path='/Users/yintianyunxi/Desktop/Malware_1D_CNN')
+# print("Extraction complete.")
+
+main('/Users/path/of/folder')
